650d6365f6fe165f60ab044ee8628b0026bfab79
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2023-12-05 03:07:11 +0200

    create phone auth cubit

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,18 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_maps/business_logic/cubit/phone_auth/phone_auth_cubit.dart';
 import 'package:flutter_maps/presentation/screens/login_screen.dart';
+import 'package:flutter_maps/presentation/screens/map_screen.dart';
 import 'package:flutter_maps/presentation/screens/otp_screen.dart';
+import 'package:bloc/bloc.dart';
 
 class AppRouter {
+  PhoneAuthCubit? phoneAuthCubit;
+
+  AppRouter() {
+    phoneAuthCubit = PhoneAuthCubit();
+  }
+
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case LoginScreen.routeName:
         return MaterialPageRoute(
-          builder: (_) =>  LoginScreen(),
+          builder: (_) => BlocProvider<PhoneAuthCubit>.value(
+            value: phoneAuthCubit!,
+            child: LoginScreen(),
+          ),
         );
 
       case OtpScreen.routeName:
         return MaterialPageRoute(
-          builder: (_) =>  OtpScreen(phoneNumber: '123'),
+          builder: (_) => BlocProvider<PhoneAuthCubit>.value(
+            value: phoneAuthCubit!,
+            child: OtpScreen(phoneNumber: '123'),
+          ),
+        );
+
+      case MapScreen.routeName:
+        return MaterialPageRoute(
+          builder: (_) => const MapScreen(),
         );
     }
     return null;
